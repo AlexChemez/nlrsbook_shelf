3cfe30d47b6dacbd80876f5ab42451dce426e5f3
Александр Иннокентьевич Чемезов <alexchemez@gmail.com>
2023-02-10 12:55:12 +0900

    Update block_nlrsbook_shelf.php

--- block_nlrsbook_shelf.php
+++ block_nlrsbook_shelf.php
@@ -1,11 +1,5 @@
 <?php
 
-require_once($CFG->dirroot . "/blocks/nlrsbook_auth/Query.php");
-
-require_login();
-
-use App\Querys\Query;
-
 class block_nlrsbook_shelf extends block_base {
 
     public function init() {
@@ -21,41 +15,12 @@ class block_nlrsbook_shelf extends block_base {
         $style = file_get_contents($CFG->dirroot . "/blocks/nlrsbook_shelf/style/nlrsbook_shelf.css");
         $js = file_get_contents($CFG->dirroot . "/blocks/nlrsbook_shelf/js/nlrsbook_shelf.js");
 
-        $seamlessAuthUserId = $USER->id; // Идентицикатор пользователя
-        $seamlessAuthOrgId = 1; // Идентификатор организации
-
-        $secret = get_config('nlrsbook_auth', 'org_private_key'); // Секретный ключ организации
-        $seamlessAuthSignature = Query::generateServerApiRequestSignature([
-            "orgId" => $seamlessAuthOrgId,
-            "userIdInEduPlatform" => $seamlessAuthUserId,
-        ], $secret);
-
-        $getToken = Query::getToken($seamlessAuthUserId, $seamlessAuthSignature); // TODO: получать из токена
-        $nlrsUserId = Query::getSub($seamlessAuthUserId); // TODO: получать из токена
-
-        $seamlessAuthSignatureBase64 = Query::generateServerApiRequestSignatureBase64([
-            "orgId" => $seamlessAuthOrgId,
-            "userIdInEduPlatform" => $nlrsUserId,
-        ], $secret);
-
-        $baseUrl = "https://e.nlrs.ru/seamless-auth-redirect?seamlessAuthOrgId=${seamlessAuthOrgId}&seamlessAuthUserId=${nlrsUserId}&seamlessAuthSignature=${seamlessAuthSignatureBase64}";
-
-        $shelfUrl = "${baseUrl}&override_redirect=https%3A%2F%2Fnlrs.ru%2Flk%2Fshelf";
-        $ordersShelfUrl = "${baseUrl}&override_redirect=https%3A%2F%2Fnew.nlrs.ru%2Flk%2Forders-shelf";
-        $ticketsUrl = "${baseUrl}&override_redirect=https%3A%2F%2Fnlrs.ru%2Flk%2Ftickets";
-
         $this->content = new stdClass;
 
         $this->content->text = <<<HTML
             <style>{$style}</style>
             <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
             <div>
-                <div class="mb-3">
-                    <a href="{$shelfUrl}" target="_blank" class="nlrsbook_shelf_card__btn btn btn-primary btn-sm mb-1">Моя полка</a>
-                    <a href="{$ordersShelfUrl}" target="_blank" class="nlrsbook_shelf_card__btn btn btn-primary btn-sm mb-1">Мои заказы</a>
-                    <a href="{$ticketsUrl}" target="_blank" class="nlrsbook_shelf_card__btn btn btn-primary btn-sm mb-1">Задать вопрос</a>
-                </div>
-                <h4 class="mb-3">Моя полка <span id="nlrsbook_shelf_count"></span></h4>
                 <div class="nlrsbook_shelf_grid row" id="nlrsbook_shelf_list">
                 </div>
             </div>
@@ -64,13 +29,6 @@ class block_nlrsbook_shelf extends block_base {
 
         return $this->content;
     }
-    
-/*
-    public function hide_header()
-    {
-        return true;
-    }
-*/
 
     function has_config()
     {
