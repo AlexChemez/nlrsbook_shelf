3f4cc9f3acf38c2392197592111f4b739b6ee264
Александр Иннокентьевич Чемезов <alexchemez@gmail.com>
2023-02-10 15:07:55 +0900

    Update block_nlrsbook_shelf.php

--- lang/ru/block_nlrsbook_shelf.php
+++ lang/ru/block_nlrsbook_shelf.php
@@ -3,5 +3,3 @@ $string['pluginname'] = 'Полка НБ РС(Я)';
 $string['nlrsbook_shelf'] = 'Полка НБ РС(Я)';
 $string['nlrsbook_shelf:addinstance'] = 'Добавить блок полки НБ РС(Я)';
 $string['nlrsbook_shelf:myaddinstance'] = 'Добавить блок полки НБ РС(Я) ко мне на страницу';
-
-$string['org_private_key'] = 'Ключ организации';
