ae066b316026b8564262ba81f3afeea1430b203d
Александр Иннокентьевич Чемезов <alexchemez@gmail.com>
2023-02-10 15:07:30 +0900

    Update block_nlrsbook_shelf.php

--- lang/en/block_nlrsbook_shelf.php
+++ lang/en/block_nlrsbook_shelf.php
@@ -3,5 +3,3 @@ $string['pluginname'] = 'Полка НБ РС(Я)';
 $string['nlrsbook_shelf'] = 'Полка НБ РС(Я)';
 $string['nlrsbook_shelf:addinstance'] = 'Add a new Полка НБ РС(Я) block';
 $string['nlrsbook_shelf:myaddinstance'] = 'Add a new Полка НБ РС(Я) to the My Moodle page';
-
-$string['org_private_key'] = 'Ключ организации';
