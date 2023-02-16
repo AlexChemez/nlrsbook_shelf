7f07ffadaf066e6f131e4dbf6f518464f30546b0
AlexChemez <alexchemez@gmail.com>
2023-02-16 12:04:09 +0900

    Update

--- ajax.php
+++ ajax.php
@@ -16,91 +16,99 @@ $first = 6; // Количество книг на страницу
 $page = optional_param('page', 1, PARAM_INT); // get запрос на получение номера страницы для пагинатора
 $remove = optional_param('remove', null, PARAM_INT); // get запрос на получение идентификатора книги для удаления с полки*/
 
-$removeBook = Query::removeBookToShelf($remove); // функия удаления книги с полки
+$setting = get_config('nlrsbook_auth', 'org_private_key'); // Секретный ключ организации
 
-$token = Query::generateServerApiRequestSignature();
-$getShelf = Query::getShelf($page, $first); // получение полки пользователя
+if ($setting) {
 
-$myShelfBooks = $getShelf['data']; // Подучение данных полки читателя
-$count = $getShelf['paginatorInfo']['total']; // Получение количества книг в полке читателя
+    $removeBook = Query::removeBookToShelf($remove); // функия удаления книги с полки
 
-if ($myShelfBooks) {
-foreach ($myShelfBooks as $key => $book) {
-    $bookUrl = Query::getUrl("online2/".$book['id']);
-    $content .= '<div class="nlrsbook_shelf_card">
-                    <div class="nlrsbook_shelf_card__img_wrapper">
-                        <div class="nlrsbook_shelf_card__img_responsive"></div>
-                        <img src="'.$book['cover_thumb_url'].'" class="nlrsbook_shelf_card__img">
-                        <div class="nlrsbook_shelf_card__dropdown dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-expanded="false">
+    $token = Query::generateServerApiRequestSignature();
+    $getShelf = Query::getShelf($page, $first); // получение полки пользователя
+
+    $myShelfBooks = $getShelf['data']; // Подучение данных полки читателя
+    $count = $getShelf['paginatorInfo']['total']; // Получение количества книг в полке читателя
+
+    if ($myShelfBooks) {
+    foreach ($myShelfBooks as $key => $book) {
+        $bookUrl = Query::getUrl("online2/".$book['id']);
+        $content .= '<div class="nlrsbook_shelf_card">
+                        <div class="nlrsbook_shelf_card__img_wrapper">
+                            <div class="nlrsbook_shelf_card__img_responsive"></div>
+                            <img src="'.$book['cover_thumb_url'].'" class="nlrsbook_shelf_card__img">
+                            <div class="nlrsbook_shelf_card__dropdown dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-expanded="false">
+                            </div>
+                            <ul class="dropdown-menu">
+                                <li><a data-remove="'.$book['id'].'" class="nlrsbook-remove dropdown-item"><i class="fa fa-trash mr-2" aria-hidden="true"></i>Убрать из полки</a></li>
+                            </ul>
                         </div>
-                        <ul class="dropdown-menu">
-                            <li><a data-remove="'.$book['id'].'" class="nlrsbook-remove dropdown-item"><i class="fa fa-trash mr-2" aria-hidden="true"></i>Убрать из полки</a></li>
-                        </ul>
-                    </div>
-                    <div class="nlrsbook_shelf_card_wrapper">
-                        <div class="order_title">
-                            <a target="_blank" href="'.$bookUrl.'" target="_blank" class="nlrsbook_shelf_card__btn btn btn-primary btn-block btn-sm">Читать</a>
+                        <div class="nlrsbook_shelf_card_wrapper">
+                            <div class="order_title">
+                                <a target="_blank" href="'.$bookUrl.'" target="_blank" class="nlrsbook_shelf_card__btn btn btn-primary btn-block btn-sm">Читать</a>
+                            </div>
+                            <div class="nlrsbook_shelf_card__title">'.$book['title'].'</div>
                         </div>
-                        <div class="nlrsbook_shelf_card__title">'.$book['title'].'</div>
-                    </div>
-                </div>';
-}
-$content .= pagination($count, $first, $page);
-} else {
-    $content .= '<div class="col-12 col-sm-12 col-md-12"><div class="alert alert-info">В вашей полке нет книг</div></div>';
-}
-
-// Переключатель страниц
-function pagination($count, $first, $page)
-{
-    $output .= "<div class=\"nlrsbook_shelf_pagination col-12\"><ul class=\"pagination pagination-sm\">";
-    $pages = ceil($count / $first);
-
-    if ($pages > 1) {
-
-        if ($page > 1) {
-            $output .= "<li class=\"page-item\"><a data-page=\"" . ($page - 1) . "\" class=\"page-link nlrsbook-page\" ><span>«</span></a></li>";
-        } else {
-            $output .= "<li class=\"page-item disabled\"><span class=\"page-link nlrsbook-page\">«</span></li>";
-        }
-
-        if (($page - 3) > 0) {
-            $output .= "<li class=\"page-item \"><a data-page=\"1\" class=\"page-link nlrsbook-page\">1</a></li>";
-        }
-        if (($page - 3) > 1) {
-            $output .= "<li class=\"page-item disabled\"><span class=\"page-link nlrsbook-page\">...</span></li>";
-        }
-
-        for ($i = ($page - 2); $i <= ($page + 2); $i++) {
-            if ($i < 1) continue;
-            if ($i > $pages) break;
-            if ($page == $i)
-                $output .= "<li class=\"page-item active\"><a data-page=\"" . ($i) . "\" class=\"page-link nlrsbook-page\" >" . $i . "</a ></li > ";
-            else
-                $output .= "<li class=\"page-item \"><a data-page=\"" . ($i) . "\" class=\"page-link nlrsbook-page\">" . $i . "</a></li>";
-        }
-
-        if (($pages - ($page + 2)) > 1) {
-            $output .= "<li class=\"page-item disabled\"><span class=\"page-link nlrsbook-page\">...</span></li>";
-        }
-        if (($pages - ($page + 2)) > 0) {
-            if ($page == $pages) {
-                $output .= "<li class=\"page-item active\"><a data-page=\"" . ($pages) . "\" class=\"page-link nlrsbook-page\" >" . $pages . "</a ></li > ";
-            } else {
-                $output .= "<li class=\"page-item \"><a data-page=\"" . ($pages) . "\" class=\"page-link nlrsbook-page\">" . $pages . "</a></li>";
-            }
-        }
-
-        if ($page < $pages) {
-            $output .= "<li class=\"page-item\"><a data-page=\"" . ($page + 1) . "\" class=\"page-link nlrsbook-page\" ><span>»</span></a></li>";
-        } else {
-            $output .= "<li class=\"page-item disabled\"><span class=\"page-link nlrsbook-page\">«</span></li>";
-        }
-
+                    </div>';
+    }
+    $content .= pagination($count, $first, $page);
+    } else {
+        $content .= '<div class="col-12 col-sm-12 col-md-12"><div class="alert alert-info">В вашей полке нет книг</div></div>';
     }
 
-    $output .= "</ul></div>";
-    return $output;
-}
+    // Переключатель страниц
+    function pagination($count, $first, $page)
+    {
+        $output .= "<div class=\"nlrsbook_shelf_pagination col-12\"><ul class=\"pagination pagination-sm\">";
+        $pages = ceil($count / $first);
 
-echo json_encode(['page' => $page, 'token' => $seamlessAuthSignatureBase64, 'count' => $count, 'remove' => $remove, 'html' => $content]);
+        if ($pages > 1) {
+
+            if ($page > 1) {
+                $output .= "<li class=\"page-item\"><a data-page=\"" . ($page - 1) . "\" class=\"page-link nlrsbook-page\" ><span>«</span></a></li>";
+            } else {
+                $output .= "<li class=\"page-item disabled\"><span class=\"page-link nlrsbook-page\">«</span></li>";
+            }
+
+            if (($page - 3) > 0) {
+                $output .= "<li class=\"page-item \"><a data-page=\"1\" class=\"page-link nlrsbook-page\">1</a></li>";
+            }
+            if (($page - 3) > 1) {
+                $output .= "<li class=\"page-item disabled\"><span class=\"page-link nlrsbook-page\">...</span></li>";
+            }
+
+            for ($i = ($page - 2); $i <= ($page + 2); $i++) {
+                if ($i < 1) continue;
+                if ($i > $pages) break;
+                if ($page == $i)
+                    $output .= "<li class=\"page-item active\"><a data-page=\"" . ($i) . "\" class=\"page-link nlrsbook-page\" >" . $i . "</a ></li > ";
+                else
+                    $output .= "<li class=\"page-item \"><a data-page=\"" . ($i) . "\" class=\"page-link nlrsbook-page\">" . $i . "</a></li>";
+            }
+
+            if (($pages - ($page + 2)) > 1) {
+                $output .= "<li class=\"page-item disabled\"><span class=\"page-link nlrsbook-page\">...</span></li>";
+            }
+            if (($pages - ($page + 2)) > 0) {
+                if ($page == $pages) {
+                    $output .= "<li class=\"page-item active\"><a data-page=\"" . ($pages) . "\" class=\"page-link nlrsbook-page\" >" . $pages . "</a ></li > ";
+                } else {
+                    $output .= "<li class=\"page-item \"><a data-page=\"" . ($pages) . "\" class=\"page-link nlrsbook-page\">" . $pages . "</a></li>";
+                }
+            }
+
+            if ($page < $pages) {
+                $output .= "<li class=\"page-item\"><a data-page=\"" . ($page + 1) . "\" class=\"page-link nlrsbook-page\" ><span>»</span></a></li>";
+            } else {
+                $output .= "<li class=\"page-item disabled\"><span class=\"page-link nlrsbook-page\">«</span></li>";
+            }
+
+        }
+
+        $output .= "</ul></div>";
+        return $output;
+    }
+
+    echo json_encode(['page' => $page, 'html' => $content]);
+} else {
+    $content = '<p>Плагин не настроен. Обратитесь к администратору образовательного учреждения.</p>';
+    echo json_encode(['page' => $page, 'html' => $content]);  
+}
