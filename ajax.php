47d59e304087da809398058a9f985bc4f9c6404f
AlexChemez <alexchemez@gmail.com>
2023-02-16 12:27:11 +0900

    Update

--- ajax.php
+++ ajax.php
@@ -54,61 +54,61 @@ if ($setting) {
         $content .= '<div class="col-12 col-sm-12 col-md-12"><div class="alert alert-info">В вашей полке нет книг</div></div>';
     }
 
-    // Переключатель страниц
-    function pagination($count, $first, $page)
-    {
-        $output .= "<div class=\"nlrsbook_shelf_pagination col-12\"><ul class=\"pagination pagination-sm\">";
-        $pages = ceil($count / $first);
-
-        if ($pages > 1) {
-
-            if ($page > 1) {
-                $output .= "<li class=\"page-item\"><a data-page=\"" . ($page - 1) . "\" class=\"page-link nlrsbook-page\" ><span>«</span></a></li>";
-            } else {
-                $output .= "<li class=\"page-item disabled\"><span class=\"page-link nlrsbook-page\">«</span></li>";
-            }
-
-            if (($page - 3) > 0) {
-                $output .= "<li class=\"page-item \"><a data-page=\"1\" class=\"page-link nlrsbook-page\">1</a></li>";
-            }
-            if (($page - 3) > 1) {
-                $output .= "<li class=\"page-item disabled\"><span class=\"page-link nlrsbook-page\">...</span></li>";
-            }
-
-            for ($i = ($page - 2); $i <= ($page + 2); $i++) {
-                if ($i < 1) continue;
-                if ($i > $pages) break;
-                if ($page == $i)
-                    $output .= "<li class=\"page-item active\"><a data-page=\"" . ($i) . "\" class=\"page-link nlrsbook-page\" >" . $i . "</a ></li > ";
-                else
-                    $output .= "<li class=\"page-item \"><a data-page=\"" . ($i) . "\" class=\"page-link nlrsbook-page\">" . $i . "</a></li>";
-            }
-
-            if (($pages - ($page + 2)) > 1) {
-                $output .= "<li class=\"page-item disabled\"><span class=\"page-link nlrsbook-page\">...</span></li>";
-            }
-            if (($pages - ($page + 2)) > 0) {
-                if ($page == $pages) {
-                    $output .= "<li class=\"page-item active\"><a data-page=\"" . ($pages) . "\" class=\"page-link nlrsbook-page\" >" . $pages . "</a ></li > ";
-                } else {
-                    $output .= "<li class=\"page-item \"><a data-page=\"" . ($pages) . "\" class=\"page-link nlrsbook-page\">" . $pages . "</a></li>";
-                }
-            }
-
-            if ($page < $pages) {
-                $output .= "<li class=\"page-item\"><a data-page=\"" . ($page + 1) . "\" class=\"page-link nlrsbook-page\" ><span>»</span></a></li>";
-            } else {
-                $output .= "<li class=\"page-item disabled\"><span class=\"page-link nlrsbook-page\">«</span></li>";
-            }
-
-        }
-
-        $output .= "</ul></div>";
-        return $output;
-    }
-
     echo json_encode(['page' => $page, 'html' => $content]);
 } else {
-    $content = '<p>Плагин не настроен. Обратитесь к администратору образовательного учреждения.</p>';
+    $content = '<div class="col-sm-12"><div class="alert alert-warning">Плагин не настроен. Обратитесь к администратору образовательного учреждения.</div></div>';
     echo json_encode(['page' => $page, 'html' => $content]);  
+}
+
+// Переключатель страниц
+function pagination($count, $first, $page)
+{
+    $output .= "<div class=\"nlrsbook_shelf_pagination col-12\"><ul class=\"pagination pagination-sm\">";
+    $pages = ceil($count / $first);
+
+    if ($pages > 1) {
+
+        if ($page > 1) {
+            $output .= "<li class=\"page-item\"><a data-page=\"" . ($page - 1) . "\" class=\"page-link nlrsbook-page\" ><span>«</span></a></li>";
+        } else {
+            $output .= "<li class=\"page-item disabled\"><span class=\"page-link nlrsbook-page\">«</span></li>";
+        }
+
+        if (($page - 3) > 0) {
+            $output .= "<li class=\"page-item \"><a data-page=\"1\" class=\"page-link nlrsbook-page\">1</a></li>";
+        }
+        if (($page - 3) > 1) {
+            $output .= "<li class=\"page-item disabled\"><span class=\"page-link nlrsbook-page\">...</span></li>";
+        }
+
+        for ($i = ($page - 2); $i <= ($page + 2); $i++) {
+            if ($i < 1) continue;
+            if ($i > $pages) break;
+            if ($page == $i)
+                $output .= "<li class=\"page-item active\"><a data-page=\"" . ($i) . "\" class=\"page-link nlrsbook-page\" >" . $i . "</a ></li > ";
+            else
+                $output .= "<li class=\"page-item \"><a data-page=\"" . ($i) . "\" class=\"page-link nlrsbook-page\">" . $i . "</a></li>";
+        }
+
+        if (($pages - ($page + 2)) > 1) {
+            $output .= "<li class=\"page-item disabled\"><span class=\"page-link nlrsbook-page\">...</span></li>";
+        }
+        if (($pages - ($page + 2)) > 0) {
+            if ($page == $pages) {
+                $output .= "<li class=\"page-item active\"><a data-page=\"" . ($pages) . "\" class=\"page-link nlrsbook-page\" >" . $pages . "</a ></li > ";
+            } else {
+                $output .= "<li class=\"page-item \"><a data-page=\"" . ($pages) . "\" class=\"page-link nlrsbook-page\">" . $pages . "</a></li>";
+            }
+        }
+
+        if ($page < $pages) {
+            $output .= "<li class=\"page-item\"><a data-page=\"" . ($page + 1) . "\" class=\"page-link nlrsbook-page\" ><span>»</span></a></li>";
+        } else {
+            $output .= "<li class=\"page-item disabled\"><span class=\"page-link nlrsbook-page\">«</span></li>";
+        }
+
+    }
+
+    $output .= "</ul></div>";
+    return $output;
 }
